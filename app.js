c5d571b2c5480df3361a8a40518d977f6b5bdb53
natedean <nate.dean@vml.com>
2015-08-07 23:34:09 -0500

    User is now able to persist firstName, lastName, and email to mongodb

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 var app = require('koa')();
 var router = require('koa-router')();
 var fs = require('co-fs');
+var parse = require('co-body');
 var mongo = require('koa-mongo');
+var moment = require('moment');
 
 app.use(mongo({
   uri: 'mongodb://localhost/simple',
@@ -11,10 +13,27 @@ app.use(mongo({
   log: false
 }));
 
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 router.get('/', function* (next){
-  this.body = yield this.mongo.db('simple').collection('people').findOne();
+  this.type = 'text/html';
+  this.body = yield fs.readFile('./index.html');
+});
+
+router.post('/add-person', function* (next){
+  var body = yield parse(this);
+
+  yield this.mongo.db('simple').collection('people').insert({
+    firstName:body.firstName,
+    lastName:body.lastName,
+    email:body.email,
+    date: moment().format("dddd, MMMM Do YYYY, h:mm:ss a")
+  });
+
+  var peopleCollection = this.mongo.db('simple').collection('people');
+  var people = yield peopleCollection.find({}).toArray();
+
+  this.body = people;
 });
 
 app.use(router.routes());
