d81f383adef90c6bc123476b9723f4df319617ea
natedean <nate.dean@vml.com>
2015-08-14 10:19:28 -0500

    Modularizing

--- app.js
+++ app.js
@@ -1,10 +1,7 @@
 var app = require('koa')();
-var router = require('koa-router')();
-var fs = require('co-fs');
-var parse = require('co-body');
 var mongo = require('koa-mongo');
 var serve = require('koa-static');
-var moment = require('moment');
+var router = require('./server/routes');
 
 var PORT = process.env.PORT || 3000;
 
@@ -17,36 +14,6 @@ app.use(mongo({
 }));
 
 app.use(serve('./public'));
-
-router.get('/', function* (next){
-  this.type = 'text/html';
-  this.body = yield fs.readFile('./views/index.html');
-});
-
-router.get('/angular-people', function* (next){
-  this.type = 'text/html';
-  this.body = yield fs.readFile('./views/angular-people.html');
-});
-
-router.get('/people', function* (next){
-  var peopleCollection = this.mongo.db('simple').collection('people');
-  var people = yield peopleCollection.find({}).toArray();
-
-  this.body = people;
-});
-
-router.post('/add-person', function* (next){
-  var body = yield parse(this);
-
-  yield this.mongo.db('simple').collection('people').insert({
-    firstName:body.firstName,
-    lastName:body.lastName,
-    email:body.email,
-    date: moment().format("dddd, MMMM Do YYYY, h:mm a")
-  });
-
-  this.redirect('/angular-people');
-});
 
 app.use(router.routes());
 
